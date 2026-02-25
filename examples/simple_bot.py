019374ee38414fda742f3e23cd895e6767b472de
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-25 11:14:36 +0300

    Update simple_bot.py

--- examples/simple_bot.py
+++ examples/simple_bot.py
@@ -1,12 +1,12 @@
 from projectz import Client
 
-projectz = Client()
-projectz.login(email="example@gmail.com", password="password")
+projz = Client()
+projz.login(email="example@gmail.com", password="password")
 
-chat_id = projectz.get_joined_chats()["list"][0]["threadId"]
-projectz.send_message(chat_id=chat_id, content="Welcome")
+chat_id = projz.get_joined_chats()["list"][0]["threadId"]
+projz.send_message(chat_id=chat_id, content="Welcome")
 
 @projectz.on()
 def event(data):
-    projectz.send_message(
+    projz.send_message(
     	data["msg"]["threadId"], f"Hello {data['author']['nickname']}")
