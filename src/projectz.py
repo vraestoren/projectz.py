e5ff25c8b29804f40312c486af59ca8a6eb6bfcf
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-24 14:49:15 +0300

    Update projectz.py

--- src/projectz.py
+++ src/projectz.py
@@ -8,318 +8,288 @@ from requests import Session
 from hashlib import sha1, sha256
 
 class Client(WebSocket):
-	def __init__(self, device_id: str = None) -> None:
-		self.api = "https://api.projz.com"
-		self.sid = None
-		self.user_id = None
-		self.device_id = self._device_id() if not device_id else device_id
-		self.session = Session()
-		self.session.headers = {
-			"rawDeviceId": self.device_id,
-			"appType": "MainApp",
-			"appVersion": "1.23.4",
-			"osType": "2",
-			"deviceType": "1",
-			"nonce": str(uuid4()),
-			"reqTime": str(int(time() * 1000)),
-			"Accept-Language": "en-US",
-			"countryCode": "EN",
-			"User-Agent": "com.projz.z.android/1.23.4-12525 (Linux; U; Android 7.1.2; SM-N975F; Build/samsung-user 7.1.2 2)",
-			"timeZone": "480",
-			"carrierCountryCodes": "en",
-			"Content-Type": "application/json; charset=UTF-8",
-			"Host": "api.projz.com",
-			"Connection": "Keep-Alive"
-		}
+    def __init__(self, device_id: str = None) -> None:
+        self.api = "https://api.projz.com"
+        self.sid = None
+        self.user_id = None
+        self.device_id = self._device_id() if not device_id else device_id
+        self.session = Session()
+        self.session.headers = {
+            "rawDeviceId": self.device_id,
+            "appType": "MainApp",
+            "appVersion": "1.23.4",
+            "osType": "2",
+            "deviceType": "1",
+            "nonce": str(uuid4()),
+            "reqTime": str(int(time() * 1000)),
+            "Accept-Language": "en-US",
+            "countryCode": "EN",
+            "User-Agent": "com.projz.z.android/1.23.4-12525 (Linux; U; Android 7.1.2; SM-N975F; Build/samsung-user 7.1.2 2)",
+            "timeZone": "480",
+            "carrierCountryCodes": "en",
+            "Content-Type": "application/json; charset=UTF-8",
+            "Host": "api.projz.com",
+            "Connection": "Keep-Alive"
+        }
 
-	def login(self, email: str, password: str) -> dict:
-		data = dumps({
-			"authType": 1,
-			"email": email,
-			"password": password
-		})
-		path = "/v1/auth/login"
-		self._sign(path=path, body=data)
-		response = self.session.post(
-			f"{self.api}{path}", data=data).json()
-		if "sId" in response:
-			self.sid = response["sId"]
-			self.session.headers["sId"] = self.sid
-			self.user_id = response["account"]["uid"]
-			self.connect()
-		return response
+    def _post(self, path: str, data: bytes = b"") -> dict:
+        self._sign(path=path, body=data)
+        return self.session.post(f"{self.api}{path}", data=data).json()
 
-	def register(
-			self,
-			email: str,
-			password: str,
-			security_code: str,
-			nickname: str,
-			tag_line: str,
-			gender: int = 1,
-			birthday: str = "1900-01-01",
-			set_credentials: bool = False) -> dict:
-		self.check_security_validation(email, security_code)
-		data = dumps({
-			"authType": 1,
-			"purpose": 1,
-			"email": email,
-			"password": password,
-			"phoneNumber": "+7 ",
-			"securityCode": security_code,
-			"invitationCode": "",
-			"secret": "",
-			"nickname": nickname,
-			"tagLine": tag_line,
-			"icon": "",
-			"nameCardBackground": "",
-			"gender": gender,
-			"birthday": birthday,
-			"requestToBeReactivated": False,
-			"countryCode": "en",
-			"suggestedCountryCode": "EN"
-		})
-		path = "/v1/auth/register"
-		self._sign(path=path, body=data)
-		return self.session.post(f"{self.api}{path}", data=data).json()
+    def _get(self, path: str) -> dict:
+        self._sign(path=path, body=b"")
+        return self.session.get(f"{self.api}{path}").json()
 
-	def send_message(
-			self,
-			chat_id: int,
-			content: str,
-			message_type: int = 1,
-			reply_message_id: int = None) -> dict:
-		data = {
-			"t": 1,
-			"threadId": chat_id,
-			"msg": {
-				"type": message_type,
-				"status": 1,
-				"threadId": chat_id,
-				"createdTime": int(time()),
-				"uid": self.user_id,
-				"seqId": int(time()),
-				"content": content,
-				"messageId": message_type,
-				"extensions": {
-					"replyMessageId": reply_message_id
-				}
-			}
-		}
+    def _delete(self, path: str) -> dict:
+        self._sign(path=path, body=b"")
+        return self.session.delete(f"{self.api}{path}").json()
 
-		return self.send_json(data)
+    def login(self, email: str, password: str) -> dict:
+        data = dumps({
+            "authType": 1,
+            "email": email,
+            "password": password
+        })
+        response = self._post("/v1/auth/login", data)
+        if "sId" in response:
+            self.sid = response["sId"]
+            self.session.headers["sId"] = self.sid
+            self.user_id = response["account"]["uid"]
+            self.connect()
+        return response
 
-	def change_password(self, old_password: str, new_password: str) -> dict:
-		data = dumps({
-			"newPassword": new_password,
-			"oldPassword": old_password
-		})
-		path = "/v1/auth/change-password"
-		self._sign(path=path, body=data)
-		return self.session.post(
-			f"{self.api}{path}", data=data).json()
+    def register(
+            self,
+            email: str,
+            password: str,
+            security_code: str,
+            nickname: str,
+            tag_line: str,
+            gender: int = 1,
+            birthday: str = "1900-01-01") -> dict:
+        self.check_security_validation(email, security_code)
+        data = dumps({
+            "authType": 1,
+            "purpose": 1,
+            "email": email,
+            "password": password,
+            "phoneNumber": "+7 ",
+            "securityCode": security_code,
+            "invitationCode": "",
+            "secret": "",
+            "nickname": nickname,
+            "tagLine": tag_line,
+            "icon": "",
+            "nameCardBackground": "",
+            "gender": gender,
+            "birthday": birthday,
+            "requestToBeReactivated": False,
+            "countryCode": "en",
+            "suggestedCountryCode": "EN"
+        })
+        return self._post("/v1/auth/register", data)
 
-	def get_recommended_circles(
-			self,
-			size: int = 10,
-			page_token: str = None) -> dict:
-		path = f"/v1/circles?type=recommend&size={size}"
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def send_message(
+            self,
+            chat_id: int,
+            content: str,
+            message_type: int = 1,
+            reply_message_id: int = None) -> dict:
+        data = {
+            "t": 1,
+            "threadId": chat_id,
+            "msg": {
+                "type": message_type,
+                "status": 1,
+                "threadId": chat_id,
+                "createdTime": int(time()),
+                "uid": self.user_id,
+                "seqId": int(time()),
+                "content": content,
+                "messageId": message_type,
+                "extensions": {
+                    "replyMessageId": reply_message_id
+                }
+            }
+        }
+        return self.send_json(data)
 
-	def get_my_circles(self, size: int = 10, page_token: str = None) -> dict:
-		path = f"/v1/circles?type=joined&categoryId=0&size={size}"
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def change_password(self, old_password: str, new_password: str) -> dict:
+        data = dumps({
+            "newPassword": new_password,
+            "oldPassword": old_password
+        })
+        return self._post("/v1/auth/change-password", data)
 
-	def get_circle_chats(
-			self,
-			circle_id: int,
-			social_id: str = None,
-			circle_link: str = None,
-			size: int = 10,
-			page_token: str = None) -> dict:
-		path = f"/v1/chat/threads?type=circle&objectId={circle_id}&size={size}",
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_recommended_circles(
+            self,
+            size: int = 10,
+            page_token: str = None) -> dict:
+        path = f"/v1/circles?type=recommend&size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def get_circle_info(self, circle_id: int) -> dict:
-		path = f"/v1/circles/{circle_id}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_my_circles(
+            self,
+            size: int = 10,
+            page_token: str = None) -> dict:
+        path = f"/v1/circles?type=joined&categoryId=0&size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def get_link_info(self, link: str) -> dict:
-		data = dumps({"link": link})
-		path = "/v1/links/path"
-		self._sign(path=path, body=data)
-		return self.session.post(
-			f"{self.api}{path}", data=data).json()
+    def get_circle_chats(
+            self,
+            circle_id: int,
+            size: int = 10,
+            page_token: str = None) -> dict:
+        path = f"/v1/chat/threads?type=circle&objectId={circle_id}&size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def join_circle(self, circle_id: int) -> dict:
-		path = f"/v1/circles/{circle_id}/members"
-		self._sign(path=path, body=b"")
-		return self.session.post(f"{self.api}{path}").json()
+    def get_circle_info(self, circle_id: int) -> dict:
+        return self._get(f"/v1/circles/{circle_id}")
 
-	def leave_circle(self, circle_id: int) -> dict:
-		path = f"/v1/circles/{circle_id}/members"
-		self._sign(path=path, body=b"")
-		return self.session.delete(f"{self.api}{path}").json()
+    def get_link_info(self, link: str) -> dict:
+        return self._post("/v1/links/path", dumps({"link": link}))
 
-	def join_chat(self, chat_id: int) -> dict:
-		path = f"/v1/chat/threads/{chat_id}/members"
-		self._sign(path=path, body=b"")
-		return self.session.post(f"{self.api}{path}").json()
+    def join_circle(self, circle_id: int) -> dict:
+        return self._post(f"/v1/circles/{circle_id}/members")
 
-	def leave_chat(self, chat_id: int) -> dict:
-		path = f"/v1/chat/threads/{chat_id}/members"
-		self._sign(path=path, body=b"")
-		return self.session.delete(f"{self.api}{path}").json()
+    def leave_circle(self, circle_id: int) -> dict:
+        return self._delete(f"/v1/circles/{circle_id}/members")
 
-	def request_security_validation(self, email: str) -> dict:
-		data = dumps({
-			"authType": 1,
-			"purpose": 1,
-			"email": email,
-			"countryCode": "en"
-		})
-		path = "/v1/auth/request-security-validation"
-		self._sign(path=path, body=data)
-		return self.session.post(
-			f"{self.api}{path}", data=data).json()
+    def join_chat(self, chat_id: int) -> dict:
+        return self._post(f"/v1/chat/threads/{chat_id}/members")
 
-	def get_chat_messages(
-			self,
-			chat_id: int,
-			size: int = 10,
-			page_token: str = None) -> dict:
-		path = f"/v1/chat/threads/{chat_id}/messages?size={size}"
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def leave_chat(self, chat_id: int) -> dict:
+        return self._delete(f"/v1/chat/threads/{chat_id}/members")
 
-	def get_joined_chats(
-			self,
-			start: int = 0,
-			size: int = 10,
-			chats_type: str = "all") -> dict:
-		path = f"/v1/chat/joined-threads?start={start}&size={size}&type={chats_type}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def request_security_validation(self, email: str) -> dict:
+        data = dumps({
+            "authType": 1,
+            "purpose": 1,
+            "email": email,
+            "countryCode": "en"
+        })
+        return self._post("/v1/auth/request-security-validation", data)
 
-	def check_security_validation(self, email: str, code: str) -> dict:
-		data = dumps({
-			"authType": 1,
-			"email": email,
-			"securityCode": code
-		})
-		path = "/v1/auth/check-security-validation"
-		self._sign(path=path, body=data)
-		return self.session.get(f"{self.api}{path}", data=data).json()
+    def check_security_validation(self, email: str, code: str) -> dict:
+        data = dumps({
+            "authType": 1,
+            "email": email,
+            "securityCode": code
+        })
+        return self._post("/v1/auth/check-security-validation", data)
 
-	def get_circle_users(
-			self,
-			circle_id: int,
-			size: int = 10,
-			page_token: str = None,
-			type: str = "normal") -> dict:
-		path = f"/v1/circles/{circle_id}/members?type={type}&size={size}"
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_chat_messages(
+            self,
+            chat_id: int,
+            size: int = 10,
+            page_token: str = None) -> dict:
+        path = f"/v1/chat/threads/{chat_id}/messages?size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def get_circle_admins(self, circle_id: int) -> dict:
-		path = f"/v1/circles/{circle_id}/management-team"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_joined_chats(
+            self,
+            start: int = 0,
+            size: int = 10,
+            chats_type: str = "all") -> dict:
+        return self._get(
+            f"/v1/chat/joined-threads?start={start}&size={size}&type={chats_type}")
 
-	def get_recommended_users(self) -> dict:
-		path = "/v1/onboarding/recommend-users"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_circle_users(
+            self,
+            circle_id: int,
+            size: int = 10,
+            page_token: str = None,
+            user_type: str = "normal") -> dict:
+        path = f"/v1/circles/{circle_id}/members?type={user_type}&size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def get_circle_active_users(
-			self,
-			circle_id: int,
-			size: int = 10,
-			page_token: str = None) -> dict:
-		path = f"/v1/circles/{circle_id}/active-members?size={size}"
-		if page_token:
-			path = f"{path}&pageToken={page_token}"
-		self._sign(path=path, body=b"")
-		return self.session.get(f"{self.api}{path}").json()
+    def get_circle_admins(self, circle_id: int) -> dict:
+        return self._get(f"/v1/circles/{circle_id}/management-team")
 
-	def visit_profile(self, user_id: int) -> dict:
-		path = f"/v1/user/profile/{user_id}/visit"
-		self._sign(path=path, body=b"")
-		return self.session.post(f"{self.api}{path}").json()
+    def get_recommended_users(self) -> dict:
+        return self._get("/v1/onboarding/recommend-users")
 
-	def start_chat(
-			self,
-			user_ids: list,
-			message: str,
-			message_type: int = 1) -> dict:
-		data = dumps({
-			"type": message_type,
-			"status": 1,
-			"background": self.get_default_chat_background().raw_json,
-			"inviteMessageContent": message,
-			"invitedUids": user_ids
-		})
-		path = "/v1/chat/threads"
-		self._sign(path=path, body=data)
-		return self.session.post(
-			f"{self.api}{path}", data=data).json()
+    def get_circle_active_users(
+            self,
+            circle_id: int,
+            size: int = 10,
+            page_token: str = None) -> dict:
+        path = f"/v1/circles/{circle_id}/active-members?size={size}"
+        if page_token:
+            path += f"&pageToken={page_token}"
+        return self._get(path)
 
-	def verify_captcha(self, captcha_value: str) -> dict:
-		data = dumps({"captchaValue": captcha_value})
-		path = "/api/f/v1/risk/verify-captcha"
-		self._sign(path=path, body=data)
-		return self.session.post(
-			f"{self.api}{path}", data=data).json()
+    def visit_profile(self, user_id: int) -> dict:
+        return self._post(f"/v1/user/profile/{user_id}/visit")
 
-	def _sign(self, path: str, body: bytes) -> None:
-		self.session.headers["nonce"] = str(uuid4())
-		self.session.headers["reqTime"] = str(int(time() * 1000))
+    def start_chat(
+            self,
+            user_ids: list,
+            message: str,
+            message_type: int = 1) -> dict:
+        data = dumps({
+            "type": message_type,
+            "status": 1,
+            "background": self.get_default_chat_background().raw_json,
+            "inviteMessageContent": message,
+            "invitedUids": user_ids
+        })
+        return self._post("/v1/chat/threads", data)
 
-		signature_data = path.encode("utf-8")
-		for signable in [
-			"rawDeviceId", "rawDeviceIdTwo",
-			"appType", "appVersion",
-			"osType", "deviceType",
-			"sId", "countryCode",
-			"reqTime", "User-Agent",
-			"contentRegion", "nonce",
-			"carrierCountryCodes"
-		]:
-			if header := self.session.headers.get(signable):
-				signature_data += header.encode("utf-8")
+    def verify_captcha(self, captcha_value: str) -> dict:
+        return self._post(
+            "/api/f/v1/risk/verify-captcha",
+            dumps({"captchaValue": captcha_value}))
 
-		if body:
-			signature_data += body
+    def _sign(self, path: str, body: bytes) -> None:
+        self.session.headers["nonce"] = str(uuid4())
+        self.session.headers["reqTime"] = str(int(time() * 1000))
 
-		mac = new(
-			bytes.fromhex(
-				"0705dd04686ef13c9228549386eb9164467fe99b284078b89ab96cb4ba6cc748"), signature_data, sha256)
-		self.session.headers["HJTRFS"] = b64encode(bytes.fromhex("02") + mac.digest()).decode("utf-8")
+        signature_data = path.encode("utf-8")
+        for signable in [
+            "rawDeviceId", "rawDeviceIdTwo",
+            "appType", "appVersion",
+            "osType", "deviceType",
+            "sId", "countryCode",
+            "reqTime", "User-Agent",
+            "contentRegion", "nonce",
+            "carrierCountryCodes"
+        ]:
+            if header := self.session.headers.get(signable):
+                signature_data += header.encode("utf-8")
 
-	def _device_id(self):
-		prefix = bytes.fromhex("02") + sha1(str(uuid4()).encode("utf-8")).digest()
-		return (prefix + sha1(
-			prefix + sha1(bytes.fromhex("c48833a8487cc749e66eb934d0ba7f2d608a")).digest()).digest()
-		).hex()
+        if body:
+            signature_data += body
 
-	def on(self, event_type: str = None):
-		def decorator(function):
-			def wrapper():
-				while True:
-					function(self.listen())
-			return wrapper
-		return decorator
+        mac = new(
+            bytes.fromhex(
+                "0705dd04686ef13c9228549386eb9164467fe99b284078b89ab96cb4ba6cc748"),
+            signature_data, sha256)
+        self.session.headers["HJTRFS"] = b64encode(
+            bytes.fromhex("02") + mac.digest()).decode("utf-8")
+
+    def _device_id(self) -> str:
+        prefix = bytes.fromhex("02") + sha1(
+            str(uuid4()).encode("utf-8")).digest()
+        return (prefix + sha1(
+            prefix + sha1(
+                bytes.fromhex("c48833a8487cc749e66eb934d0ba7f2d608a")).digest()
+        ).digest()).hex()
+
+    def on(self, event_type: str = None):
+        def decorator(function):
+            def wrapper():
+                while True:
+                    function(self.listen())
+            return wrapper
+        return decorator
